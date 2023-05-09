b9d73c2565229825aca9476121bcd61c8a600393
dileeka1998 <dileeka7777@gmail.com>
2023-05-09 11:50:31 +0530

    implement react-router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,29 @@
 import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+
+
 const Header = () => {
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
           <Container>
-            <Navbar.Brand href="/">
+            <LinkContainer to="/">
+            <Navbar.Brand>
               DBay
               </Navbar.Brand>
+              </LinkContainer>
             <Navbar.Toggle aria-controls='basic-navbar-nav'/>
             <Navbar.Collapse id='basic-navbar-nav'>
                 <Nav className='ms-auto'>
-                    <Nav.Link href='/cart'><FaShoppingCart/>Cart</Nav.Link>
-                    <Nav.Link href='/login'><FaShoppingCart/>Sign In</Nav.Link>
+                  <LinkContainer to="/cart">
+                      <Nav.Link>
+                      <FaShoppingCart/>Cart
+                      </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <Nav.Link><FaShoppingCart/>Sign In</Nav.Link>
+                  </LinkContainer>
                 </Nav> 
             </Navbar.Collapse>
           </Container>  
